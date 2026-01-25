44646d66de0ca7ec30d24d725550bc6a17cf2b9d
Dayezzz <adiasmuhsin1206@gmail.com>
2026-01-25 10:49:05 +0700

    style(text): change year in footer using function Date

--- src/components/views/footer/footer.jsx
+++ src/components/views/footer/footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer() {
+  const year = new Date().getFullYear();
   return (
     <>
       <footer className="w-full gradiasi-hijau text-white font-outfit shadow-[0_-10px_20px] shadow-black/10 h-fit py-4 flex justify-center items-center">
-        <h2>© 2025 ExploreNgalam. All rights reserved.</h2>
+        <h2>© {year} Explore Ngalam. Semua hak dilindungi.</h2>
       </footer>
     </>
   );
